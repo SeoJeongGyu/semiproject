8c79085e98d0722a94ffe531024b93f3ff9f33b2
Main <Main@DESKTOP-5HE6PP9>
2018-02-14 11:43:16 +0900

    aaaa

---
+++ semiProject/src/semi/controller/MemberController.java
@@ -0,0 +1,27 @@
+package semi.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member.do")
+public class MemberController  extends HttpServlet{
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cmd = req.getParameter("cmd");
+        if(cmd.equals("join")) {
+            req.setAttribute("page", "/member/join.jsp");
+            RequestDispatcher rd = req.getRequestDispatcher("main.jsp");
+            rd.forward(req, resp);
+        }
+    }
+    public void join(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+    
+}
---
+++ semiProject/src/semi/dao/MemberDao.java
@@ -0,0 +1,11 @@
+package semi.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MemberDao {
+    public int join() {
+       
+        return 1;
+    }
+}
