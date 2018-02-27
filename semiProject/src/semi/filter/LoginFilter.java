2d4020a833b497e7428d39264628e21a1629775f
Main <Main@DESKTOP-5HE6PP9>
2018-02-27 20:22:58 +0900

    sad

--- semiProject/src/semi/filter/LoginFilter.java
+++ semiProject/src/semi/filter/LoginFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-@WebFilter(urlPatterns = {"/sell.do","/review.do"})
+@WebFilter(urlPatterns = {"/sell.do","/review.do","/buy.do"})
 public class LoginFilter implements Filter{
     @Override
     public void destroy() {}
